878c103e9965e29a3e442e282d4b9981de69af73
jsp <jspenkar@gmail.com>
2015-10-24 18:54:17 -0500

    Dragonborn class added.

---
+++ races/dragonborn.rb
@@ -0,0 +1,33 @@
+require_relative 'race'
+class Dragonborn < RaceClass
+  attr_accessor :damage, :breath
+
+  def initialize(subclass = false)
+    super
+    @strength += 2
+    @charisma += 1
+    @movement += 30
+    self.dragon if subclass
+    # self.send(subclass) if subclass
+  end
+
+  def self.subclasses
+    return 'black, blue, brass, bronze, copper, golden, green, red, silver, white'
+  end
+
+  def dragon
+    case @subclass
+      when 'black' then @damage, @breath = 'acid', '5 by 30ft line, dexterity'
+      when 'blue' then @damage, @breath = 'lightning', '5 by 30ft line, dexterity'
+      when 'brass' then @damage, @breath = 'fire', '5 by 30ft line, dexterity'
+      when 'bronze' then @damage, @breath = 'lightning', '5 by 30ft line, dexterity'
+      when 'copper' then @damage, @breath = 'acid', '5 by 30ft line, dexterity'
+      when 'golden' then @damage, @breath = 'fire', '15 ft cone, dexterity'
+      when 'green' then @damage, @breath = 'poison', '15 ft cone, constitution'
+      when 'red' then @damage, @breath = 'fire', '15 ft cone, dexterity'
+      when 'silver' then @damage, @breath = 'cold', '15 ft cone, constitution'
+      when 'white' then @damage, @breath = 'cold', '15 ft cone, constitution'
+      else @damage, @breath = 'none', 'none'
+    end
+  end
+end
