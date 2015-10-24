d71b847c187a5296888424853660e23927edd50d
jsp <jspenkar@gmail.com>
2015-10-24 17:47:17 -0500

    Default race base added settings abilities, subclasses and score checks.

--- races/race.rb
+++ races/race.rb
@@ -1,4 +1,21 @@
 class RaceClass
-  def intialize
+  attr_accessor :strength, :dexterity, :wisdom, :intelligence, :constitution, :charisma, :vision, :movement
+  def initialize(subclass = false)
+    @strength = 0
+    @dexterity = 0
+    @wisdom = 0
+    @intelligence = 0
+    @constitution = 0
+    @charisma = 0
+    @vision = 'normal'
+    @movement = 0
+  end
+
+  def subclasses
+    return 'hilldwarf, mountaindwarf'
+  end
+
+  def ability_scores
+    return {strength: @strength, dexterity: @dexterity, wisdom: @wisdom, intelligence: @intelligence, constitution: @constitution, charisma: @charisma}
   end
 end
