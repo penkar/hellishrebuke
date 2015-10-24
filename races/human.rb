93c36f290c99d4a0c2578c863c07ac44fd199d88
jsp <jspenkar@gmail.com>
2015-10-24 18:18:49 -0500

    Human class added.

---
+++ races/human.rb
@@ -0,0 +1,18 @@
+require_relative 'race'
+class Human < RaceClass
+  def initialize(subclass = false)
+    super
+    @strength = 1
+    @dexterity = 1
+    @wisdom = 1
+    @intelligence = 1
+    @constitution = 1
+    @charisma = 1
+    @speed += 30
+    self.send(subclass) if false #subclass
+  end
+
+  def subclasses
+    return 'none'
+  end
+end
