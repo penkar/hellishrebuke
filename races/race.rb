9014bb785de7d5166aa499aef5a2f19e423dca9f
jsp <jspenkar@gmail.com>
2015-10-24 18:54:52 -0500

    Subclass added to raceclass attributes

--- races/race.rb
+++ races/race.rb
@@ -1,6 +1,7 @@
 class RaceClass
-  attr_accessor :strength, :dexterity, :wisdom, :intelligence, :constitution, :charisma, :vision, :movement, :toughness, :stealth
+  attr_accessor :strength, :dexterity, :wisdom, :intelligence, :constitution, :charisma, :vision, :movement, :toughness, :stealth, :subclass
   def initialize(subclass = false)
+    @subclass = subclass
     @strength = 0
     @dexterity = 0
     @wisdom = 0
@@ -14,7 +15,7 @@ class RaceClass
   end
 
   def self.subclasses
-    return 'dwarf, elf, halfling, human'
+    return 'dwarf, elf, halfling, human, dragonborn'
   end
 
   def ability_scores
--- races/races.rb
+++ races/races.rb
@@ -1,2 +1,7 @@
 require_relative 'elf'
 require_relative 'dwarf'
+require_relative 'halfling'
+require_relative 'human'
+require_relative 'dragonborn'
+require_relative 'dwarf'
+require_relative 'dwarf'
