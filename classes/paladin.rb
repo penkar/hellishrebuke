29028094fb8de3e3563740bc6a4d9b601c57c01b
jsp <jspenkar@gmail.com>
2015-10-25 09:51:05 -0500

    Paladin class added.

--- classes/paladin.rb
+++ classes/paladin.rb
@@ -1,13 +1,13 @@
 require_relative 'class'
-class Monk < Classes
+class Bard < Classes
   def initialize(skill1, skill2)
     super
-    hash = {acrobatics: true, athletics: true, history: true, insight: true, religion: true, stealth: true}
+    hash = {athletics: true, insight: true, intimidation: true, medicine: true, persuasion: true, religion: true}
     @skills.push(skill1.to_sym) if hash[skill1.to_sym]
     @skills.push(skill2.to_sym) if hash[skill2.to_sym]
   end
 
   def self.skills
-    return hash = "acrobatics, athletics, history, insight, religion, stealth"
+    return "athletics, insight, intimidation, medicine, persuasion, religion"
   end
 end
