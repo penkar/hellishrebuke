5166b14325e0f432a668109776ec885a6f7bde23
jsp <jspenkar@gmail.com>
2015-10-25 10:15:18 -0500

    Wizard class added.

---
+++ classes/wizard.rb
@@ -0,0 +1,13 @@
+require_relative 'class'
+class Wizard < Classes
+  def initialize(skill1, skill2)
+    super
+    hash = {arcana: true, history: true, insight: true, investigation: true, medicine: true, religion: true}
+    @skills.push(skill1.to_sym) if hash[skill1.to_sym]
+    @skills.push(skill2.to_sym) if hash[skill2.to_sym]
+  end
+
+  def self.skills
+    return "arcana, history, insight, investigation, medicine, religion"
+  end
+end
