6a2c37145e2dedf66cb7c429f29a82c048a85a29
jsp <jspenkar@gmail.com>
2015-10-25 00:06:52 -0500

    Classes improved.

--- classes/barbarian.rb
+++ classes/barbarian.rb
@@ -1,6 +1,15 @@
 require_relative 'class'
 class Barbarian < Classes
-  def initialize(subclass = false)
+  def initialize(skill1, skill2)
     super
+    hash = {animal_handling: true, athletics: true, intimidation: true, nature: true, perception: true, survival: true}
+    @skills.push(skill1.to_sym) if hash[skill1.to_sym]
+    @skills.push(skill2.to_sym) if hash[skill2.to_sym]
+  end
+
+  def self.skills
+    return "animal_handling, athletics, intimidation, nature, perception, survival"
   end
 end
+#hash = {acrobatics: true, animal_handling: true, arcana: true, athletics: true, deception: true, history: true, insight: true, intimidation: true, investigation: true, medicine: true, nature: true, perception: true, performance: true, persuasion: true, religion: true, sleight_of_hand: true, stealth: true, survival: true}
+#"acrobatics, animal_handling, arcana, athletics, deception, history, insight, intimidation, investigation, medicine, nature, perception, performance, persuasion, religion, sleight_of_hand, stealth, survival"
--- classes/class.rb
+++ classes/class.rb
@@ -1,16 +1,23 @@
 class Classes
-  attr_accessor :strength, :dexterity, :wisdom, :intelligence, :constitution, :charisma
-  def initialize
-    @strength = 0
-    @dexterity = 0
-    @wisdom = 0
-    @intelligence = 0
-    @constitution = 0
-    @charisma = 0
+  attr_accessor :skills, :hitpoints
+  # :strength, :dexterity, :wisdom, :intelligence, :constitution, :charisma, 
+  def initialize(skill1, skill2)
+    # @strength = 0
+    # @dexterity = 0
+    # @wisdom = 0
+    # @intelligence = 0
+    # @constitution = 0
+    # @charisma = 0
+    @skills = []
+    @hitpoints = 0
   end
 
   def self.subclasses
     return 'Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard'
+  end
+
+  def get_skills
+    @skills
   end
 
 end
