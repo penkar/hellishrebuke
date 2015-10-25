0165ca22339ff51343ca070632e01038625d75a8
jsp <jspenkar@gmail.com>
2015-10-25 10:04:32 -0500

    Correction to bard

--- classes/bard.rb
+++ classes/bard.rb
@@ -1,10 +1,11 @@
 require_relative 'class'
 class Bard < Classes
-  def initialize(skill1, skill2)
+  def initialize(skill1, skill2, skill3)
     super
     hash = {acrobatics: true, animal_handling: true, arcana: true, athletics: true, deception: true, history: true, insight: true, intimidation: true, investigation: true, medicine: true, nature: true, perception: true, performance: true, persuasion: true, religion: true, sleight_of_hand: true, stealth: true, survival: true}
     @skills.push(skill1.to_sym) if hash[skill1.to_sym]
     @skills.push(skill2.to_sym) if hash[skill2.to_sym]
+    @skills.push(skill3.to_sym) if hash[skill3.to_sym]
   end
 
   def self.skills
