84487f35b75814bef6e47e883b6148d68411b38a
jsp <jspenkar@gmail.com>
2015-10-24 21:56:30 -0500

    Tiefling class added.

---
+++ races/tiefling.rb
@@ -0,0 +1,26 @@
+require_relative 'race'
+class Tiefling < RaceClass
+  def initialize(subclass = false)
+    super
+    @intelligence += 1
+    @charisma += 2
+    @movement += 30
+    @vision = 'superior dark'
+    @resistance = 'fire'
+    self.send(subclass) if subclass
+  end
+
+  def self.subclasses
+    return 'lightfoot, stout'
+  end
+
+  def lightfoot
+    @charisma += 1
+    @stealth = 'naturally stealthy'
+  end
+
+  def stout
+    @constitution += 1
+    @toughness = 'Stout Resilience'
+  end
+end
