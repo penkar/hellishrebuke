4fd4c3433c4b2001632b7913332ff6d923eeb030
jsp <jspenkar@gmail.com>
2015-10-24 23:30:22 -0500

    Corrections to tiefling subclasses.

--- races/tiefling.rb
+++ races/tiefling.rb
@@ -7,20 +7,9 @@ class Tiefling < RaceClass
     @movement += 30
     @vision = 'superior dark'
     @resistance = @resistance.push('fire')
-    self.send(subclass) if subclass
   end
 
   def self.subclasses
-    return 'lightfoot, stout'
-  end
-
-  def lightfoot
-    @charisma += 1
-    @stealth = 'naturally stealthy'
-  end
-
-  def stout
-    @constitution += 1
-    @toughness = 'Stout Resilience'
+    return 'None'
   end
 end
